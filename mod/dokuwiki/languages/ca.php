f4befae72104349bd35649366bfa78c3f8cb880c
Jordi Brunet <jbrus77@cryptolab.net>
2014-10-02 12:16:14 +0200

    Update ca.php

--- mod/dokuwiki/languages/ca.php
+++ mod/dokuwiki/languages/ca.php
@@ -8,11 +8,11 @@ $language = array (
   'dokuwiki:dokuwiki' => 'Wiki',
   'dokuwiki:group' => 'Espai wiki',
   'dokuwiki:userwiki' => 'La meva Wiki',
-  'dokuwiki:sidebar' => 'Wiki: Navegació',
+  'dokuwiki:sidebar' => 'Wiki: Navegaci&oacute;',
   'groups:enabledokuwiki' => 'Habilita la wiki del grup',
-  'groups:enabledokuwiki_frontpage' => 'Mostra la pàgina d\'inici de la wiki a la pàgina principal del grup (només si la wiki està activada)',
-  'groups:enabledokuwiki_frontsidebar' => 'Mostra la barra lateral de la wiki a la pàgina principal del grup (només si la wiki està activada)',
+  'groups:enabledokuwiki_frontpage' => 'Mostra la p&agrave;gina d\'inici de la wiki a la p&agrave;gina principal del grup (nom&eacute;s si la wiki est&agrave; activada)',
+  'groups:enabledokuwiki_frontsidebar' => 'Mostra la barra lateral de la wiki a la p&agrave;gina principal del grup (nom&eacute;s si la wiki est&agrave; activada)',
   'dokuwiki' => 'Wikis',
-  'dokuwiki:river:modified' => 'En/Na %s ha modificat la pàgina %s el %s',
+  'dokuwiki:river:modified' => 'En/Na %s ha modificat la p&agrave;gina %s el %s',
 );
 add_translation("ca", $language);
