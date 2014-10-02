e175136e48c6eee80aa226fe39ad8d5532f8848e
Jordi Brunet <jbrus77@cryptolab.net>
2014-10-02 12:20:36 +0200

    Update ca.php

--- mod/embed/languages/ca.php
+++ mod/embed/languages/ca.php
@@ -3,8 +3,8 @@ $language = array (
   'embed:embed' => 'Adjuncions',
   'embed:media' => 'Adjuntar contingut',
   'embed:instructions' => 'Clica sobre l\'arxiu que vulguis adjuntar al contingut',
-  'embed:upload' => 'Carrega un mèdia',
-  'embed:upload_type' => 'Tipus de càrrega:',
+  'embed:upload' => 'Carrega un m&egrave;dia',
+  'embed:upload_type' => 'Tipus de c&agrave;rrega:',
   'embed:no_upload_content' => 'No hi ha contingut per penjar!',
   'embed:no_section_content' => 'No es troba cap element',
 );
